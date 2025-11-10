54a4d41998865fe0295c3180314520af948c4cea
Todor Krushkov <todorkrushkov.1304@gmail.com>
2025-11-10 12:12:03 +0200

    create Role Enum class

--- src/main/java/telerik/project/models/Role.java
+++ src/main/java/telerik/project/models/Role.java
@@ -1,4 +1,13 @@
 package telerik.project.models;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
 public enum Role {
+    USER("User"),
+    ADMIN("Administrator");
+
+    private final String displayName;
 }
