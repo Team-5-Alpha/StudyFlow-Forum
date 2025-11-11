180a9529e881c5f902711506e80bf5d07ca436f7
Todor Krushkov <todorkrushkov.1304@gmail.com>
2025-11-11 15:19:00 +0200

    update UserRepository
    make user repository consistent with other repositories

--- src/main/java/telerik/project/repositories/UserRepositoryImpl.java
+++ src/main/java/telerik/project/repositories/UserRepositoryImpl.java
@@ -12,23 +12,21 @@ import telerik.project.repositories.contracts.UserRepository;
 import java.util.List;
 
 @Repository
-public class UserRepositoryImp implements UserRepository {
+public class UserRepositoryImpl implements UserRepository {
 
     private final SessionFactory sessionFactory;
 
     @Autowired
-    public UserRepositoryImp(SessionFactory sessionFactory) {
+    public UserRepositoryImpl(SessionFactory sessionFactory) {
         this.sessionFactory = sessionFactory;
     }
 
     @Override
     public User getById(Long id) {
         try(Session session = sessionFactory.openSession()) {
-            User user = session.get(User.class, id);
-            if (user == null) {
-                throw new EntityNotFoundException("User");
-            }
-            return user;
+            return session.byId(User.class)
+                    .loadOptional(id)
+                    .orElseThrow(() -> new EntityNotFoundException("id"));
         }
     }
 
@@ -43,27 +41,20 @@ public class UserRepositoryImp implements UserRepository {
     @Override
     public User getByUsername(String username) {
         try(Session session = sessionFactory.openSession()) {
-            Query<User> query = session.createQuery("from User where username = :username", User.class);
-            query.setParameter("username", username);
-
-            List<User> result = query.list();
-            if (result.isEmpty()) {
-                throw new EntityNotFoundException("username");
-            }
-            return result.get(0);
+            return session.createQuery("from User where username = :username", User.class)
+                    .setParameter("username", username)
+                    .uniqueResultOptional()
+                    .orElseThrow(() -> new EntityNotFoundException("username"));
         }
     }
 
     @Override
     public User getByEmail(String email) {
         try(Session session = sessionFactory.openSession()) {
-            Query<User> query = session.createQuery("from User where email = :email", User.class);
-            query.setParameter("email", email);
-            List<User> result = query.list();
-            if (result.isEmpty()) {
-                throw new EntityNotFoundException("email");
-            }
-            return result.get(0);
+            return session.createQuery("from User where email = :email", User.class)
+                    .setParameter("email", email)
+                    .uniqueResultOptional()
+                    .orElseThrow(() -> new EntityNotFoundException("email"));
         }
     }
 
