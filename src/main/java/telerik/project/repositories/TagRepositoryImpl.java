69abb696f8e1995eb375c28c8571bb33fffee91a
Todor Krushkov <todorkrushkov.1304@gmail.com>
2025-11-11 14:38:41 +0200

    create Tag repository

--- src/main/java/telerik/project/repositories/TagRepositoryImpl.java
+++ src/main/java/telerik/project/repositories/TagRepositoryImpl.java
@@ -1,4 +1,78 @@
 package telerik.project.repositories;
 
-public class TagRepositoryImpl {
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import telerik.project.exceptions.EntityNotFoundException;
+import telerik.project.models.Tag;
+import telerik.project.repositories.contracts.TagRepository;
+
+import java.util.List;
+
+@Repository
+public class TagRepositoryImpl implements TagRepository {
+
+    private final SessionFactory sessionFactory;
+
+    @Autowired
+    public TagRepositoryImpl(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
+    }
+
+    @Override
+    public Tag getById(Long id) {
+        try(Session session = sessionFactory.openSession()) {
+            return session.byId(Tag.class)
+                    .loadOptional(id)
+                    .orElseThrow(() -> new EntityNotFoundException("id"));
+        }
+    }
+
+    @Override
+    public Tag getByName(String name) {
+        try(Session session = sessionFactory.openSession()) {
+            return session.createQuery("from Tag where name = :name", Tag.class)
+                    .setParameter("name", name)
+                    .uniqueResultOptional()
+                    .orElseThrow(() -> new EntityNotFoundException("name"));
+        }
+    }
+
+    @Override
+    public List<Tag> getAll() {
+        try(Session session = sessionFactory.openSession()) {
+            Query<Tag> query = session.createQuery("from Tag", Tag.class);
+            return query.list();
+        }
+    }
+
+    @Override
+    public void create(Tag tag) {
+        try(Session session = sessionFactory.openSession()) {
+            session.beginTransaction();
+            session.persist(tag);
+            session.getTransaction().commit();
+        }
+    }
+
+    @Override
+    public void update(Tag tag) {
+        try(Session session = sessionFactory.openSession()) {
+            session.beginTransaction();
+            session.merge(tag);
+            session.getTransaction().commit();
+        }
+    }
+
+    @Override
+    public void delete(Long id) {
+        Tag tagToDelete = getById(id);
+        try(Session session = sessionFactory.openSession()) {
+            session.beginTransaction();
+            session.remove(tagToDelete);
+            session.getTransaction().commit();
+        }
+    }
 }
--- src/main/java/telerik/project/repositories/contracts/TagRepository.java
+++ src/main/java/telerik/project/repositories/contracts/TagRepository.java
@@ -1,4 +1,19 @@
 package telerik.project.repositories.contracts;
 
+import telerik.project.models.Tag;
+
+import java.util.List;
+
 public interface TagRepository {
+    Tag getById(Long id);
+
+    Tag getByName(String name);
+
+    List<Tag> getAll();
+
+    void create(Tag tag);
+
+    void update(Tag tag);
+
+    void delete(Long id);
 }
