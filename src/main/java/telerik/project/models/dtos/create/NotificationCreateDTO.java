11426ae9a98529a87ea337745d2aa8d2b4570497
Binev7 <georgi_binev.gogo@abv.bg>
2025-11-16 00:13:03 +0200

    create NotficationCreateDTO

--- src/main/java/telerik/project/models/dtos/create/NotificationCreateDTO.java
+++ src/main/java/telerik/project/models/dtos/create/NotificationCreateDTO.java
@@ -1,4 +1,26 @@
 package telerik.project.models.dtos.create;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class NotificationCreateDTO {
+
+    @NotNull(message = "recipientId is required.")
+    Long recipientId;
+
+    @NotNull(message = "actorId is required.")
+    Long actorId;
+
+    @NotNull(message = "entityId is required.")
+    Long entityId;
+
+    @NotBlank(message = "entityType cannot be empty.")
+    String entityType;
+
+    @NotBlank(message = "actionType cannot be empty.")
+    String actionType;
 }
