d9b125dc51f5afa8c5926cc5d2df1cf12657387f
Binev7 <georgi_binev.gogo@abv.bg>
2025-11-15 21:58:32 +0200

    create TagCreateDTO

--- src/main/java/telerik/project/models/dtos/create/TagCreateDTO.java
+++ src/main/java/telerik/project/models/dtos/create/TagCreateDTO.java
@@ -1,4 +1,14 @@
 package telerik.project.models.dtos.create;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class TagCreateDTO {
+
+    @NotBlank(message = "name cannot be empty.")
+    private String name;
 }
