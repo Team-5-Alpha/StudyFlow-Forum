9d48cb9a27a1cd737b7cfb55df378c9162daaf9a
Binev7 <georgi_binev.gogo@abv.bg>
2025-11-15 21:48:40 +0200

    create CommentResponseDTO

--- src/main/java/telerik/project/models/dtos/response/CommentResponseDTO.java
+++ src/main/java/telerik/project/models/dtos/response/CommentResponseDTO.java
@@ -1,4 +1,31 @@
 package telerik.project.models.dtos.response;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
 public class CommentResponseDTO {
+
+    private Long id;
+
+    private String content;
+
+    private LocalDateTime createdAt;
+
+    private LocalDateTime updatedAt;
+
+    private Boolean isDeleted;
+
+    private Long authorId;
+
+    private String authorUsername;
+
+    private Long postId;
+
+    private Long parentCommentId;
+
+    private long likeCount;
 }
