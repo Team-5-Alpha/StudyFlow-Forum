887aff388c01a1b51a97501f180fabad482b0e63
Binev7 <georgi_binev.gogo@abv.bg>
2025-11-16 12:45:26 +0200

    create UserResponseDTO

--- src/main/java/telerik/project/models/dtos/response/UserResponseDTO.java
+++ src/main/java/telerik/project/models/dtos/response/UserResponseDTO.java
@@ -1,4 +1,33 @@
 package telerik.project.models.dtos.response;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
 public class UserResponseDTO {
+
+    private Long id;
+
+    private String firstName;
+
+    private String lastName;
+
+    private String username;
+
+    private String email;
+
+    private String role;
+
+    private String phoneNumber;
+
+    private String profilePhotoURL;
+
+    private Boolean isBlocked;
+
+    private LocalDateTime createdAt;
+
+    private LocalDateTime updatedAt;
 }
