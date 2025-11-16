b57e425ed4378e389c8fbd043c32de9dbb4621cc
Binev7 <georgi_binev.gogo@abv.bg>
2025-11-16 12:35:29 +0200

    create UserUpdateDTO

--- src/main/java/telerik/project/models/dtos/update/UserUpdateDTO.java
+++ src/main/java/telerik/project/models/dtos/update/UserUpdateDTO.java
@@ -1,4 +1,20 @@
 package telerik.project.models.dtos.update;
 
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class UserUpdateDTO {
+
+    @NotBlank(message = "firstName cannot be empty.")
+    String firstName;
+
+    @NotBlank(message = "lastName cannot be empty.")
+    String lastName;
+
+    String phoneNumber;
+
+    String profilePhotoURL;
 }
