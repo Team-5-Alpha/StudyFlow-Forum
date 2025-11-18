96606b067a40e8af600a9eac8d70aeb1c45baf60
Todor Krushkov <todorkrushkov.1304@gmail.com>
2025-11-18 17:01:35 +0200

    create NotificationFactory

--- src/main/java/telerik/project/helpers/NotificationFactory.java
+++ src/main/java/telerik/project/helpers/NotificationFactory.java
@@ -1,4 +1,27 @@
 package telerik.project.helpers;
 
-public class NotificationFactory {
+import telerik.project.models.Notification;
+import telerik.project.models.User;
+
+import java.time.LocalDateTime;
+
+public final class NotificationFactory {
+
+    private NotificationFactory() {}
+
+    public static Notification cretae(
+            User actor,
+            User recipient,
+            Long entityId,
+            String entityType,
+            String actionType
+    ) {
+        Notification notification = new Notification();
+        notification.setActor(actor);
+        notification.setRecipient(recipient);
+        notification.setEntityId(entityId);
+        notification.setEntityType(entityType);
+        notification.setActionType(actionType);
+        return notification;
+    }
 }
