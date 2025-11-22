b308d4eff00eb084c6ae9a7df2f1e2e643bd75b6
Todor Krushkov <todorkrushkov.1304@gmail.com>
2025-11-22 15:09:06 +0200

    create UserController & optimize filtration

--- src/main/java/telerik/project/models/User.java
+++ src/main/java/telerik/project/models/User.java
@@ -25,14 +25,14 @@ public class User {
     @Column(name = "user_id")
     private Long id;
 
+    @Column(name = "username")
+    private String username;
+
     @Column(name = "first_name")
     private String firstName;
 
     @Column(name = "last_name")
     private String lastName;
-
-    @Column(name = "username")
-    private String username;
 
     @Column(name = "email")
     private String email;
